3519b63b58639fa5bd960c9e23098b4e6b4f70f6
Phasexd <adhil751012@gmail.com>
2024-06-08 21:38:58 +0530

    main

---
+++ server/index.ts
@@ -0,0 +1,10 @@
+import app from './app'
+
+const server = Bun.serve({
+  port: process.env.PORT || 3001,
+  hostname: "0.0.0.0",
+  fetch: app.fetch
+  }
+
+);
+console.log("server is running", server.port)
