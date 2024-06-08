2ab129b47606a73e5457e4fd0baa342c34583759
Phasexd <adhil751012@gmail.com>
2024-06-08 22:20:34 +0530

    tested

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,7 @@
 import {Hono} from 'hono';
 import {logger} from 'hono/logger'
 import { serveStatic } from 'hono/bun'
-import { itemsRoutes } from './Routes/items';
+import { itemsRoutes } from './app/items';
 import { cors } from 'hono/cors'
 import { handle } from 'hono/vercel'
 import type { PageConfig } from 'next'
